36d2128ac2b843c2fc1aaf72c5343dfbdcc0c52f
kkasper1 <karol.kasperek@pw.edu.pl>
2021-11-28 23:14:18 +0100

    change password

--- CityGym/CityGym/app/src/main/java/com/example/citygym/MainActivity.java
+++ CityGym/CityGym/app/src/main/java/com/example/citygym/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.citygym;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.citygym.databinding.FragmentLoginBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -17,6 +21,8 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.citygym.databinding.ActivityMainBinding;
 import com.example.citygym.databinding.FragmentLoginBinding;
+
+import org.w3c.dom.Text;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -73,5 +79,76 @@ public class MainActivity extends AppCompatActivity {
         QR.setVisibility(View.INVISIBLE);
         ImageView Avatar = findViewById(R.id.Avatar);
         Avatar.setVisibility(View.INVISIBLE);
+        TextView UserName = findViewById((R.id.UsernameView));
+        UserName.setVisibility((View.VISIBLE));
+        TextView PasswordView = findViewById(R.id.PasswordView);
+        PasswordView.setVisibility((View.VISIBLE));
+        TextView EmailView = findViewById(R.id.emailView);
+        EmailView.setVisibility(View.VISIBLE);
+        TextView SubId = findViewById(R.id.SubscriptionID);
+        SubId.setVisibility(View.VISIBLE);
+        TextView SubEnd = findViewById(R.id.SubscriptionEnd);
+        SubEnd.setVisibility(View.VISIBLE);
+        Button ChangePass = findViewById(R.id.ChangePassword);
+        ChangePass.setVisibility(View.VISIBLE);
+    }
+
+    public void change_password (View view){
+        TextView UserName = findViewById(R.id.UsernameView);
+        UserName.setVisibility(View.INVISIBLE);
+        TextView PasswordView = findViewById(R.id.PasswordView);
+        PasswordView.setVisibility(View.INVISIBLE);
+        TextView EmailView = findViewById(R.id.emailView);
+        EmailView.setVisibility(View.INVISIBLE);
+        TextView SubId = findViewById(R.id.SubscriptionID);
+        SubId.setVisibility(View.INVISIBLE);
+        TextView SubEnd = findViewById(R.id.SubscriptionEnd);
+        SubEnd.setVisibility(View.INVISIBLE);
+        Button ChangePass = findViewById(R.id.ChangePassword);
+        ChangePass.setVisibility(View.INVISIBLE);
+        TextView OldPass = findViewById(R.id.OldPassword);
+        OldPass.setVisibility(View.VISIBLE);
+        TextView NewPass = findViewById(R.id.NewPassword);
+        NewPass.setVisibility(View.VISIBLE);
+        TextView RepNewPass = findViewById(R.id.RepeatNewPassword);
+        RepNewPass.setVisibility(View.VISIBLE);
+        EditText GiveOldPassword = findViewById(R.id.GiveOldPassword);
+        GiveOldPassword.setVisibility(View.VISIBLE);
+        EditText GiveNewPassword = findViewById(R.id.GiveNewPassword);
+        GiveNewPassword.setVisibility(View.VISIBLE);
+        EditText GiveRepeatNewPassword = findViewById(R.id.GiveRepeatNewPassword);
+        GiveRepeatNewPassword.setVisibility(View.VISIBLE);
+        Button Change = findViewById(R.id.change);
+        Change.setVisibility(View.VISIBLE);
+    }
+
+    public void accept (View view) {
+        TextView UserName = findViewById((R.id.UsernameView));
+        UserName.setVisibility((View.VISIBLE));
+        TextView PasswordView = findViewById(R.id.PasswordView);
+        PasswordView.setVisibility((View.VISIBLE));
+        TextView EmailView = findViewById(R.id.emailView);
+        EmailView.setVisibility(View.VISIBLE);
+        TextView SubId = findViewById(R.id.SubscriptionID);
+        SubId.setVisibility(View.VISIBLE);
+        TextView SubEnd = findViewById(R.id.SubscriptionEnd);
+        SubEnd.setVisibility(View.VISIBLE);
+        Button ChangePass = findViewById(R.id.ChangePassword);
+        ChangePass.setVisibility(View.VISIBLE);
+        TextView OldPass = findViewById(R.id.OldPassword);
+        OldPass.setVisibility(View.GONE);
+        TextView NewPass = findViewById(R.id.NewPassword);
+        NewPass.setVisibility(View.GONE);
+        TextView RepNewPass = findViewById(R.id.RepeatNewPassword);
+        RepNewPass.setVisibility(View.GONE);
+        EditText GiveOldPassword = findViewById(R.id.GiveOldPassword);
+        GiveOldPassword.setVisibility(View.GONE);
+        EditText GiveNewPassword = findViewById(R.id.GiveNewPassword);
+        GiveNewPassword.setVisibility(View.GONE);
+        EditText GiveRepeatNewPassword = findViewById(R.id.GiveRepeatNewPassword);
+        GiveRepeatNewPassword.setVisibility(View.GONE);
+        Button Change = findViewById(R.id.change);
+        Change.setVisibility(View.GONE);
+        Toast.makeText(getApplicationContext(), "Haslo zostalo zmienione!", Toast.LENGTH_SHORT).show();
     }
 }
