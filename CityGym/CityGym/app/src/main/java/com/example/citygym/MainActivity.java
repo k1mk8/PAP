f8a99f3633bc933d96ef41738ec07a36002b4854
sdyszews <barbardian@wp.pl>
2021-11-28 19:41:53 +0100

    App v0.2

--- CityGym/CityGym/app/src/main/java/com/example/citygym/MainActivity.java
+++ CityGym/CityGym/app/src/main/java/com/example/citygym/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.citygym.databinding.FragmentLoginBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,28 +16,19 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.citygym.databinding.ActivityMainBinding;
+import com.example.citygym.databinding.FragmentLoginBinding;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        FragmentLoginBinding binding;
+        binding = FragmentLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
-
     }
 
     public void showQR(View view){
@@ -61,19 +53,25 @@ public class MainActivity extends AppCompatActivity {
         Avatar.setVisibility(View.VISIBLE);
     }
     public void login(View view){
-        TextView Login_Text = findViewById(R.id.Login_text);
-        Login_Text.setVisibility(View.GONE);
-        TextView Login_Input = findViewById(R.id.Login_Input);
-        Login_Input.setVisibility(View.GONE);
-        TextView Password_Text = findViewById(R.id.Password_text);
-        Password_Text.setVisibility(View.GONE);
-        TextView Password_Input = findViewById(R.id.Password_Input);
-        Password_Input.setVisibility(View.GONE);
-        View Login_Button = findViewById(R.id.Login_button);
-        Login_Button.setVisibility(View.GONE);
+        ActivityMainBinding binding;
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_login, R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    public void show_data_panel(View view){
         ImageView QR = findViewById(R.id.QRView);
-        QR.setVisibility(View.VISIBLE);
+        QR.setVisibility(View.INVISIBLE);
         ImageView Avatar = findViewById(R.id.Avatar);
-        Avatar.setVisibility(View.VISIBLE);
+        Avatar.setVisibility(View.INVISIBLE);
     }
 }
