da59271f94c3bce5b74bc8bad8f7b3a91dfe9ed5
Bartek Szymanski <bartekszymanski@barteks-macbook-pro.home>
2022-01-25 23:42:58 +0100

    working registration, authentication, events

--- CityGym/CityGym/app/src/main/java/login/LoginModule.java
+++ CityGym/CityGym/app/src/main/java/login/LoginModule.java
@@ -17,22 +17,29 @@ import java.sql.Statement;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Headers;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 
 
 public class LoginModule{
-	
+
 	public static String message = null;
 	public static String StoredLogin;
 	private static String connectionUrl = "jdbc:oracle:thin:@//ora4.ii.pw.edu.pl:1521/pdb1.ii.pw.edu.pl";
 	private static OkHttpClient client = new OkHttpClient();
+	private static String API_Address = "192.168.1.17";
 	//private static final String[] expected_password = {null};
 
 
-
+	/**
+	 *
+	 * @param login
+	 * @return
+	 */
 
 	private static String getPersonFromAPI(String login){
 
@@ -40,7 +47,7 @@ public class LoginModule{
 		String JSONresponse = null;
 
 		Request request = new Request.Builder()
-				.url("http://192.168.1.17:8080/getperson?login="+ login)
+				.url("http://"+API_Address+":8080/getperson?login="+ login)
 				.build();
 		try (Response response = client.newCall(request).execute()) {
 			if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
@@ -56,6 +63,69 @@ public class LoginModule{
 		return JSONresponse;
 	}
 
+
+	/**
+	 *
+	 * @param imie
+	 * @param nazwisko
+	 * @param PESEL
+	 * @param data_urodzenia
+
+	 * @param login
+	 * @param haslo
+	 * @return
+	 */
+	 public static Boolean register(String kraj, String miasto, String kod_pocztowy, String ulica,
+									String nr_budynku, String nr_mieszkania,int region,String imie,
+									String nazwisko, int PESEL, String data_urodzenia,
+									String login, String haslo, String email){
+
+		 final Boolean[] registration_result = {false};
+
+
+		 Thread loginThread = new Thread(new Runnable() {
+			 @Override
+
+
+			 public void run() {
+
+				 MediaType mediaType = MediaType.parse("text/plain");
+				 RequestBody body = RequestBody.create(mediaType, "");
+
+
+
+				 Request request = new Request.Builder()
+						 .url("http://"+API_Address+":8080/register?kraj="+kraj+"&miasto="+miasto+"&kod_pocztowy="+kod_pocztowy+"&ulica="+ulica+
+								 "&nr_budynku="+nr_budynku+"&nr_mieszkania="+nr_mieszkania+"&region="+region+"&imie="+imie+"&nazwisko="+nazwisko+"&PESEL="+PESEL+
+								 "&data_urodzenia="+data_urodzenia+"&login="+login+"&haslo="+haslo+"&email="+email)
+						 .method("POST", body)
+						 .build();
+
+
+				 try (Response response = client.newCall(request).execute()) {
+					 if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+					 System.out.println("WEWNATRZ REJESTRACJI");
+					 registration_result[0] = Boolean.parseBoolean(response.body().string());
+				 }
+				 catch (IOException e){
+					 e.printStackTrace();
+				 }
+			 }
+		 });
+
+		 loginThread.start();
+
+		 try {
+			 loginThread.join();
+		 } catch (InterruptedException e) {
+			 e.printStackTrace();
+		 }
+
+		 return registration_result[0];
+
+
+	}
 
 
     /**
@@ -78,24 +148,24 @@ public class LoginModule{
 		}
 
 
-		Thread loginThread = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				person[0] = getPersonFromAPI(login);
+			Thread loginThread = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					person[0] = getPersonFromAPI(login);
+				}
+			});
+
+			loginThread.start();
+
+			try {
+				loginThread.join();
+				JSONObject jsPassword = new JSONObject(person[0]);
+				expected_password[0] = jsPassword.getString("password");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
-		});
-
-		loginThread.start();
-
-		try {
-			loginThread.join();
-			JSONObject jsPassword = new JSONObject(person[0]);
-			expected_password[0] = jsPassword.getString("password");
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		catch (JSONException j){
-			j.printStackTrace();
+			catch (JSONException j){
+				j.printStackTrace();
 		}
 
 			System.out.println("Po wywolaniu getPasswordFromAPI " + expected_password[0]);
@@ -138,10 +208,93 @@ public class LoginModule{
 	}
 
 
-//	public static Boolean registerUser(String login, String password){
-//
-//	}
+	public static String getEventByID(int event_id){
 
-	  
+		final String[] event = {null};
+
+
+		Thread loginThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Request request = new Request.Builder()
+						.url("http://"+API_Address+":8080/getEventByID?event_id="+ event_id)
+						.build();
+				try (Response response = client.newCall(request).execute()) {
+					if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+					event[0] = response.body().string();
+
+				}
+				catch (IOException e){
+					e.printStackTrace();
+				}
+			}
+		});
+
+		loginThread.start();
+
+		try {
+			loginThread.join();
+			JSONObject jsEvent = new JSONObject(event[0]);
+			event[0] = jsEvent.toString();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		catch (JSONException j){
+			j.printStackTrace();
+		}
+
+
+
+		return event[0];
+
+
+	}
+
+
+	public static String getEventByDesc(String opis){
+
+		final String[] event = {null};
+
+
+		Thread loginThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Request request = new Request.Builder()
+						.url("http://"+API_Address+":8080/getEventByDesc?opis="+ opis)
+						.build();
+				try (Response response = client.newCall(request).execute()) {
+					if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+					event[0] = response.body().string();
+
+				}
+				catch (IOException e){
+					e.printStackTrace();
+				}
+			}
+		});
+
+		loginThread.start();
+
+		try {
+			loginThread.join();
+			JSONObject jsEvent = new JSONObject(event[0]);
+			event[0] = jsEvent.toString();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		catch (JSONException j){
+			j.printStackTrace();
+		}
+
+
+
+		return event[0];
+
+
+	}
+
+
 }	
 
